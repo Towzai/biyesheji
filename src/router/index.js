0f803ba62ce5942cd73780a47149da32c2ca4e06
Tow_z <1247953486+Towzai@users.noreply.github.com>
2020-03-12 11:18:30 +0800

    嘿嘿嘿

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import contact from '@/components/contact/contact_index'
+import news from '@/components/news/news_index'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/contact',
       name: 'contact',
       component: contact
+    },
+    {
+      path: '/news',
+      name: 'news',
+      component: news
     }
   ]
 })
